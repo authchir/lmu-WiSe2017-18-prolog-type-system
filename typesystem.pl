ec5a2bf742d631e51e402e2575e4976b76387f6c
Sebastian Sturm <sebasian-sturm@hotmail.com>
2018-02-11 15:20:09 +0100

    fixed logical type variable bugs
    before the change the definition could be more spezific then the
    annotated type.
    Also bugs in the reinstantiation of type variables where fixed

--- typesystem.pl
+++ typesystem.pl
@@ -12,15 +12,34 @@ mylen(L, Len) :- L = [_|T], mylen(T, TLen), Len is TLen + 1.
 typeannot(X, int) :- integer(X).
 typeannot(p, [int, int]).
 typeannot(q, [int, int]).
-typeannot(mylen, [list(_A), int]).
+typeannot(mylen, [list(tyvar(_A)), int]).
 
-new_inst_type_list([], []).
-new_inst_type_list([X|Xs], [Y|Ys]) :- new_inst_type(X, Y), new_inst_type_list(Xs, Ys).
+% tyvar predicate ensure that the definition is not more spezial
+% than die type annotation
+new_inst_tyvars(InTy, OutTy) :- 
+   new_inst_tyvars_list([InTy], [OutTy]).
+new_inst_tyvars_list(InTys, OutTys) :- 
+   new_inst_tyvars_list_cxt(_, InTys, OutTys).
 
-new_inst_type(A, B) :- var(A), !, B = _.
-new_inst_type(int, int).
-new_inst_type(unit, unit).
-new_inst_type(list(A), list(B)) :- new_inst_type(A, B).
+new_inst_tyvars_list_cxt(_Cxt, [], []).
+new_inst_tyvars_list_cxt(Cxt, [In|InS], [Out|OutS]) :- 
+   new_inst_tyvars_cxt(Cxt, In, Out), 
+   new_inst_tyvars_list_cxt(Cxt, InS, OutS).
+
+new_inst_tyvars_cxt(Cxt, tyvar(In), Out) :-
+  tyvar_cxt_lookup(Cxt, tyvar(In), Out).
+new_inst_tyvars_cxt(_Cxt, int, int).
+new_inst_tyvars_cxt(_Cxt, unit, unit).
+new_inst_tyvars_cxt(Cxt, list(In), list(Out)) :- 
+   new_inst_tyvars_cxt(Cxt, In, Out).
+
+tyvar_cxt_lookup(Context, tyvar(In), Out) :-
+   var(Context), !, Out = _NewVar,
+   Context = [(tyvar(In), Out)| _NewContext].
+tyvar_cxt_lookup([(tyvar(In1), Out1)|_Cxt], tyvar(In2), Out2) :-
+   In1 == In2, !, Out1 = Out2.
+tyvar_cxt_lookup([(tyvar(In1), _)|Cxt], tyvar(In2), Out) :-
+  In1 \== In2, !, context_write(Cxt, tyvar(In2), Out).
 
 context_write(Context, Y, T) :-
   var(Context), !, Context = [(Y, T)| _NewContext].
@@ -81,7 +100,7 @@ hastype(Context, A, unit) :-
   A =.. [B|Params], atom(B), !,
   write("hastype predicate"), nl,
   typeannot(B, Ty),
-  new_inst_type_list(Ty, Ty2),
+  new_inst_tyvars_list(Ty, Ty2),
   checkparams(Context, Params, Ty2).
 
 
